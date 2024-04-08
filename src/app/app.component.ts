0189d0788ed895866ce04200e99b54381d835dab
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-08 19:06:19 +0530

    form async validators with commented comment given by ai about binding this keyword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -36,4 +37,27 @@ export class AppComponent implements OnInit {
     }
     return null;
   }
+
+  forbiddenEmails(control:FormControl):Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'dmakwana651@rku.ac.in') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+    return promise;
+  }
 }
+
+/*
+>>> this is the binding this keyword for names and emails, answer given by ai
+
+In the provided code snippet, the forbiddenNames function is being passed as a validator to the 'username' FormControl, and the forbiddenEmails function is being passed as a validator to the 'email' FormControl.
+
+When passing this.forbiddenNames.bind(this) as a validator to the 'username' FormControl, the bind(this) part explicitly binds the this keyword of the forbiddenNames function to the class instance. This is necessary because forbiddenNames is a regular function, and without binding this, it would not have access to the class properties.
+
+On the other hand, when passing this.forbiddenEmails as a validator to the 'email' FormControl, the this keyword is not explicitly bound using bind(this). This is because forbiddenEmails is using an arrow function inside the Promise constructor, which automatically captures the this value from the surrounding code (in this case, the class instance). So, there is no need to explicitly bind this in this case
+*/
